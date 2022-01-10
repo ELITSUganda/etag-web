139d16071361cf935dfdcaec54ef4d4e4ac9495c
Muhindo Mubaraka <mubs0x@gmail.com>
2022-01-10 14:37:11 +0300

    new changes

--- app/Http/Controllers/ApiLoginController.php
+++ app/Http/Controllers/ApiLoginController.php
@@ -10,7 +10,67 @@ class ApiLoginController extends Controller
 {
     public function create_account(Request $request)
     {
-        return $request;
+        if (
+            $request->username == null ||
+            $request->phone_number == null ||
+            $request->name == null ||
+            $request->password == null
+        ) {
+            return Utils::response([
+                'status' => 0,
+                'message' => "You must provide username, password, name and phone number."
+            ]);
+        }
+
+        $u = Administrator::where('username',$request->username)->first();
+        if($u != null){
+            return Utils::response([
+                'status' => 0,
+                'message' => "User with same username already exist."
+            ]);
+        }
+        $u = Administrator::where('email',$request->username)->first();
+        if($u != null){
+            return Utils::response([
+                'status' => 0,
+                'message' => "User with same email address already exist."
+            ]);
+        }
+        $u = Administrator::where('email',$request->phone_number)->first();
+        if($u != null){
+            return Utils::response([
+                'status' => 0,
+                'message' => "User with same phone number already exist."
+            ]);
+        }
+
+        $user = new Administrator();
+        $user->name = $request->name;
+        $user->username = $request->username;
+        $user->phone_number = $request->phone_number;
+        $user->password = password_hash(trim($request->password),PASSWORD_DEFAULT);
+        if(!$user->save()){
+            return Utils::response([
+                'status' => 0,
+                'message' => "Failed to create ACCOUNT."
+            ]);
+        }
+        
+        $user->save();
+        $u = Administrator::where('username',$request->username)->first();
+        if($u === null){
+            return Utils::response([
+                'status' => 0,
+                'message' => "Failed to create account. Plase try agains."
+            ]);
+        }
+
+        return Utils::response([
+            'status' => 1,
+            'message' => "Account created successfuo.",
+            'data' => $u
+        ]);
+
     }
 
     public function index(Request $request)
@@ -18,11 +78,12 @@ class ApiLoginController extends Controller
         if (
             $request->username == null ||
             $request->password == null
-        )
+        ) {
             return Utils::response([
                 'status' => 0,
                 'message' => "You must provide both username and password."
             ]);
+        }
 
         $user = Administrator::where("username", trim($request->username))->first();
         if ($user == null) {
@@ -39,7 +100,7 @@ class ApiLoginController extends Controller
             ]);
         }
 
-        if(password_verify(trim($request->password),$user->password)){
+        if (password_verify(trim($request->password), $user->password)) {
             unset($user->password);
             $user->roles = $user->roles;
             return Utils::response([
