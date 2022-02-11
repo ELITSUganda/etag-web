8cd02e4e72530c79da15bf9fe158552e9ec58b7b
Muhindo Mubaraka <mubs0x@gmail.com>
2022-02-11 10:25:40 +0300

    anjane

--- app/Http/Controllers/ApiLoginController.php
+++ app/Http/Controllers/ApiLoginController.php
@@ -87,7 +87,7 @@ class ApiLoginController extends Controller
         ) {
             return Utils::response([
                 'status' => 0,
-                'message' => "You must provide both username and password.",
+                'message' => "You must provide both username and password. anjane",
                 $request
             ]);
         }
