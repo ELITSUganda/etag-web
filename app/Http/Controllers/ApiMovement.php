6e97712512da26c49ec6c9c5a98059d83488ee60
Muhindo Mubaraka <mubs0x@gmail.com>
2022-02-19 10:22:18 +0300

    slaughter

--- app/Http/Controllers/ApiMovement.php
+++ app/Http/Controllers/ApiMovement.php
@@ -33,7 +33,7 @@ class ApiMovement extends Controller
         ){
             $items = Movement::paginate(1000)->withQueryString()->items();
         } else if($user->role == 'slaughter'){
-            $items = Movement::where(['destination_slaughter_house' , '=',  $user_id])->get();
+            $items = Movement::where('destination_slaughter_house' , '=',  $user_id)->get();
         }else{
             $items = Movement::where(['administrator_id' => $user_id])->get();
         }
