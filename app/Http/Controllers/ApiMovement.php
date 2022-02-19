3666eac5ffecf6240c816e1b8a739c3974765c52
Muhindo Mubaraka <mubs0x@gmail.com>
2022-02-19 06:33:05 +0300

    updated_at

--- app/Http/Controllers/ApiMovement.php
+++ app/Http/Controllers/ApiMovement.php
@@ -40,7 +40,7 @@ class ApiMovement extends Controller
         foreach ($items as $key => $value) {
             $value->created_at =  Carbon::parse($value->created_at)->toFormattedDateString();
             $value->updated_at =  Carbon::parse($value->created_at)->toFormattedDateString();
-            $filtered_items[] = $value;
+           // $filtered_items[] = $value;
         }
 
         return $filtered_items;
