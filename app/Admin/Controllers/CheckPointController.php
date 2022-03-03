8a5053641e18b84b3834ab510dea6fbc7c80a23c
Muhindo Mubaraka <muhindomubaraka@iut-dhaka.edu>
2022-03-03 17:22:37 +0300

    anjane

---
+++ app/Admin/Controllers/CheckPointController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\CheckPoint;
+use App\Models\SubCounty;
+use Encore\Admin\Auth\Database\Administrator;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class CheckPointController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'CheckPoint';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new CheckPoint());
+
+        $grid->column('id', __('Id'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+        $grid->column('name', __('Name'));
+        $grid->column('details', __('Details'));
+        $grid->column('longitude', __('Longitude'));
+        $grid->column('latitube', __('Latitube'));
+        $grid->column('administrator_id', __('Administrator id'));
+        $grid->column('sub_county_id', __('Sub county id'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(CheckPoint::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+        $show->field('name', __('Name'));
+        $show->field('details', __('Details'));
+        $show->field('longitude', __('Longitude'));
+        $show->field('latitube', __('Latitube'));
+        $show->field('administrator_id', __('Administrator id'));
+        $show->field('sub_county_id', __('Sub county id'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new CheckPoint());
+
+        $sub_counties = [];
+        foreach (SubCounty::all() as $key => $p) {
+            $sub_counties[$p->id] = $p->name . ", " .
+                $p->district->name . ".";
+        }
+        foreach (Administrator::all() as $key => $v) {
+            if (!$v->isRole('check-point-officer')) {
+                continue;
+            }
+            $admins[$v->id] = $v->name . " - " . $v->id . " - ({$v->username})";
+        }
+
+
+        $form->text('name', __('Checkpoinnt Name/Route'))->required();
+
+        $form->select('sub_county_id', __('Sub-county'))
+            ->options($sub_counties)
+            ->required();
+
+        $form->select('administrator_id', __('Chech-point officer'))
+            ->options($admins)
+            ->required();
+
+        $form->latlong('latitube', 'longitude', 'Location in map')->height(300);
+        $form->textarea('details', __('Details'))->required();
+
+        return $form;
+    }
+}
