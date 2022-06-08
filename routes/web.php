f6f58305ed979f25f86489f0353117a68c9f1c79
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-06-09 01:51:17 +0300

    romina

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,29 @@ Route::match(['get', 'post'], '/print2', [PrintController::class, 'index']);
 Route::match(['get', 'post'], '/print', [PrintController2::class, 'index']);
 
 Route::get('/', function () {
-    header( "Location: ". admin_url());
+
+
+    $file = null;
+    $file_name = "1.xls";
+
+    if (file_exists('./public/storage/' . $file_name)) {
+        $file = './public/storage/' . $file_name;
+    }
+
+    if ($file == null) {
+        die("not found");
+        return;
+    }
+
+    
+
+    die("File found");
+
+
+
+    die("Romina");
+
+    header("Location: " . admin_url());
     die();
     return view('welcome');
 });
