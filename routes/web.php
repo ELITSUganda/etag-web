576050afff5d8ed80a6abb9e03897d5c61b0c127
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-06-09 11:37:39 +0300

    romina

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\PrintController;
 use App\Http\Controllers\PrintController2;
 use Encore\Admin\Grid\Tools\Header;
 use Illuminate\Support\Facades\Route;
-use Excel;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,9 +20,6 @@ Route::match(['get', 'post'], '/print2', [PrintController::class, 'index']);
 Route::match(['get', 'post'], '/print', [PrintController2::class, 'index']);
 
 Route::get('/', function () {
-    $array = Excel::toArray([], 'public/storage/files/1.xls');
-    die("use Excel;"); 
-
     header( "Location: ". admin_url());
     die();
     return view('welcome');
