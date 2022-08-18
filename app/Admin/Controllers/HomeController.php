810ec63cfdcbc5955d32b1edff2db085e9ac2f7d
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-08-18 23:19:57 +0300

    phpinfo()

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -23,9 +23,7 @@ class HomeController extends Controller
     public function index(Content $content)
     {
 
-        echo "<pre>";
-        print_r(phpinfo());
-        die();
+        
         if (
             Admin::user()->isRole('administrator') ||
 
