1ad5457ffece44749ebbe216607a7bd81bc7834e
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-08-25 19:31:04 +0300

    $uri_segments

--- app/Admin/Controllers/FormDrugStockApprovalController.php
+++ app/Admin/Controllers/FormDrugStockApprovalController.php
@@ -138,7 +138,7 @@ class FormDrugStockApprovalController extends AdminController
             $uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
             $uri_segments = explode('/', $uri_path);
             $id = ((int)($uri_segments[4]));
-            dd($id);
+            dd($uri_segments);
             $form_data = FormDrugStockApproval::find($id);
 
             if ($form_data == null) {
