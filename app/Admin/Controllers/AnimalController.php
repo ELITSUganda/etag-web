1eb9677bc97f013ed2e46f7faa58ccae322600fb
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-09-08 22:53:20 +0300

    done

--- app/Admin/Controllers/AnimalController.php
+++ app/Admin/Controllers/AnimalController.php
@@ -129,7 +129,6 @@ class AnimalController extends AdminController
         });
 
         $grid->model()->orderBy('id', 'DESC');
-        $grid->column('id', __('ID'))->sortable();
         $grid->column('e_id', __('E-ID'))->sortable();
         $grid->column('v_id', __('V-ID'))->sortable();
         $grid->column('lhc', __('LHC'))->sortable();
