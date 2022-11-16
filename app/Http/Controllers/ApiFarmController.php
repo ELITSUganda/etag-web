bb38b6f9188efe4d1afbdd009841f6e858c06d23
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-11-16 17:50:22 +0300

    1024M

--- app/Http/Controllers/ApiFarmController.php
+++ app/Http/Controllers/ApiFarmController.php
@@ -14,6 +14,7 @@ class ApiFarmController extends Controller
     public function locations(Request $request)
     {
 
+        ini_set('memory_limit', '1024M'); // or you could use 1G
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
