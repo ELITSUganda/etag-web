c01151e8204e17427944ca671ba7f87e3cef6d25
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-11-19 23:46:00 +0300

    description

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -194,7 +194,7 @@ class Event extends Model
 
 
             if ($model->description == null || (strlen($model->description) < 2)) {
-                $model->description = $model->detail;
+                //$model->description = $model->detail;
             }
             if ($model->detail == null || (strlen($model->detail) < 2)) {
                 $model->detail = $model->description;
