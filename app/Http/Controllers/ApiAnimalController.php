3724fbcf754e6ac62c0ea815afe1704b42d9be22
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-12-01 02:59:20 +0300

    request

--- app/Http/Controllers/ApiAnimalController.php
+++ app/Http/Controllers/ApiAnimalController.php
@@ -112,7 +112,7 @@ class ApiAnimalController extends Controller
         //Utils::process_images_in_backround();
         return Utils::response([
             'status' => 1,
-            'data' => json_encode($request),
+            'data' => json_encode($_POST),
             'message' => "File uploaded successfully.",
         ]);
     }
