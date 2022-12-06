f26a5d56042c30e289ed430ec5f6559d089d52d8
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-12-06 15:26:55 +0300

    balance_text

--- app/Models/FinanceCategory.php
+++ app/Models/FinanceCategory.php
@@ -15,7 +15,6 @@ class FinanceCategory extends Model
     {
         parent::boot();
         self::creating(function ($m) {
-
             $name = trim($m->name);
             $administrator_id = ((int)(trim($m->administrator_id)));
             $resp = FinanceCategory::where([
@@ -23,8 +22,49 @@ class FinanceCategory extends Model
                 'administrator_id' => $administrator_id,
             ])->first();
             if ($resp != null) {
-                throw new Exception('You alreafy have account with same name.');
+                throw new Exception('You alreafy have a financial account with same name.');
             }
         });
     }
+
+    public function getBalanceAttribute()
+    {
+        return Transaction::where([
+            'finance_category_id' => $this->id
+        ])->sum('amount');
+    }
+
+    public function getBalanceTextAttribute()
+    {
+        return "UGX " . number_format($this->balance);
+    }
+    public function getTotalIncomeAttribute()
+    {
+        $tot = Transaction::where([
+            'finance_category_id' => $this->id
+        ])->where(
+            'amount',
+            '>',
+            0
+        )->sum('amount');
+
+        return "UGX " . number_format($tot);
+    }
+    public function getTotalExpenceAttribute()
+    {
+        $tot = Transaction::where([
+            'finance_category_id' => $this->id
+        ])->where(
+            'amount',
+            '<',
+            0
+        )->sum('amount');
+
+        return "UGX " . number_format($tot);
+    }
+    protected $appends = [
+        'balance_text',
+        'total_income',
+        'total_expence',
+    ];
 }
