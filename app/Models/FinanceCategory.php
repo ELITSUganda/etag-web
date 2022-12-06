8dd4ce12ac8f1151a52ab4712e52930c5b42097e
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-12-06 14:01:49 +0300

    account with same name

--- app/Models/FinanceCategory.php
+++ app/Models/FinanceCategory.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FinanceCategory extends Model
 {
     use HasFactory;
+
+
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($m) {
+
+            $name = trim($m->name);
+            $administrator_id = ((int)(trim($m->administrator_id)));
+            $resp = FinanceCategory::where([
+                'name' => $name,
+                'administrator_id' => $administrator_id,
+            ])->first();
+            if ($resp != null) {
+                throw new Exception('You alreafy have account with same name.');
+            }
+        });
+    }
 }
