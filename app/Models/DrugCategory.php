6c313ad7937bf53c347c0d5894675322088aaffc
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-12-07 02:51:01 +0300

    Drug category

--- app/Models/DrugCategory.php
+++ app/Models/DrugCategory.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,6 +12,21 @@ class DrugCategory extends Model
 
     public function category()
     {
-        return $this->belongsTo(DrugCategory::class,'drug_category_id');
+        return $this->belongsTo(DrugCategory::class, 'drug_category_id');
+    }
+
+
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($m) {
+            $name = trim($m->name);
+            $resp = FinanceCategory::where([
+                'name' => $name,
+            ])->first();
+            if ($resp != null) {
+                throw new Exception('Drug category with same name already exist.');
+            }
+        });
     }
 }
