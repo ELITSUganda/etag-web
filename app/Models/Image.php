65a5db06783a0e13d1ec8b44dbc8c3302e79b58a
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2023-01-05 01:17:56 +0300

    Update Image.php

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -13,7 +13,7 @@ class Image extends Model
     use HasFactory;
     protected $fillable = [
         'administrator_id',
-        'src', 
+        'src',
         'thumbnail',
         'parent_id',
         'size',
@@ -27,64 +27,68 @@ class Image extends Model
         parent::boot();
 
         self::deleting(function ($m) {
- 
-            $src = Utils::docs_root() ."/storage/images/".$m->src; 
 
-            if($m->thumbnail != null){
-                if(strlen($m->thumbnail) > 2){
-                    $thumb = Utils::docs_root() ."/storage/images/".$m->thumbnail; 
+            $src = Utils::docs_root() . "/storage/images/" . $m->src;
+
+            if ($m->thumbnail != null) {
+                if (strlen($m->thumbnail) > 2) {
+                    $thumb = Utils::docs_root() . "/storage/images/" . $m->thumbnail;
                 }
             }
-            if(!isset($thumb)){
-                $thumb =  Utils::docs_root() ."/storage/images/thumb_".$m->src; 
+            if (!isset($thumb)) {
+                $thumb =  Utils::docs_root() . "/storage/images/thumb_" . $m->src;
             }
 
-            if(file_exists($src)){
-                unlink($src);  
-            }  
-            if(file_exists($thumb)){ 
-                unlink($thumb);  
-            }    
+            if (file_exists($src)) {
+                unlink($src);
+            }
+            if (file_exists($thumb)) {
+                unlink($thumb);
+            }
         });
- 
+
         self::created(function ($m) {
-            $m->create_thumbail();   
+            $m->create_thumbail();
         });
-    } 
+    }
 
-    public function getSrcAttribute($src){
+    public function getSrcAttribute($src)
+    {
 
-        $source = Utils::docs_root() ."/storage/images/".$src; 
-        if(!file_exists($source)){
+        $source = Utils::docs_root() . "/storage/images/" . $src;
+        if (!file_exists($source)) {
             return 'logo.png';
-        } 
-    }    
-    public function getThumbnailAttribute($src){
+        }
+        return $source;
+    }
+    public function getThumbnailAttribute($src)
+    {
 
-        $source = Utils::docs_root() ."/storage/images/".$src; 
-        if(!file_exists($source)){ 
+        $source = Utils::docs_root() . "/storage/images/" . $src;
+        if (!file_exists($source)) {
             return 'logo.png';
-        }  
-    }    
+        }
+        return $source;
+    }
 
-    public function create_thumbail(){ 
-        $src = $this->src; 
-        $source = Utils::docs_root() ."/storage/images/".$this->src; 
-        if(!file_exists($source)){ 
-            $this->delete(); 
+    public function create_thumbail()
+    {
+        $src = $this->src;
+        $source = Utils::docs_root() . "/storage/images/" . $this->src;
+        if (!file_exists($source)) {
+            $this->delete();
             return;
-        } 
-        $target = Utils::docs_root() ."/storage/images/thumb_".$this->src;
-        
+        }
+        $target = Utils::docs_root() . "/storage/images/thumb_" . $this->src;
+
         Utils::create_thumbail([
             'source' => $source,
             'target' => $target
-        ]); 
+        ]);
 
-        if(file_exists($target)){
-           $this->thumbnail = "thumb_".$this->src;
-           $this->save(); 
-         } 
-       
+        if (file_exists($target)) {
+            $this->thumbnail = "thumb_" . $this->src;
+            $this->save();
+        }
     }
 }
