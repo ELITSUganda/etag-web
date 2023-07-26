ab11cd9cfa947b95d2a299be43d7294c8578e85e
Muhindo Mubaraka <mubs0x@gmail.com>
2023-07-26 19:55:39 +0300

    new app

--- app/Http/Controllers/ApiResurceController.php
+++ app/Http/Controllers/ApiResurceController.php
@@ -233,7 +233,7 @@ class ApiResurceController extends Controller
             //throw $th;
         }
         $manifest['last_update'] = Utils::my_date_time(Carbon::now());
-        $manifest['app_vision'] = 16;
+        $manifest['app_vision'] = 17;
 
         $data[] = $manifest;
         return Utils::response([
