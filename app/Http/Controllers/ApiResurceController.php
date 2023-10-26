911a2acea03cce7e86abc9b88599c3b0e03276e6
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2023-10-26 23:29:47 +0300

    new chnages

--- app/Http/Controllers/ApiResurceController.php
+++ app/Http/Controllers/ApiResurceController.php
@@ -281,7 +281,7 @@ class ApiResurceController extends Controller
             //throw $th;
         }
         $manifest['last_update'] = Utils::my_date_time(Carbon::now());
-        $manifest['app_vision'] = 18;
+        $manifest['app_vision'] = 20;
 
         $data[] = $manifest;
         return Utils::response([
