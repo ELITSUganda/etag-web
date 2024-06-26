f6b6146d2a512b931d379e0fec407f7b77338633
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-06-26 15:46:24 +0300

    as

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,54 +1,45 @@
-<title>Random GPS Markers in Uganda</title>
-<script
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC9Mdls_ETVjOb_u5bjcqavSI4E8S1D2Vs&callback=initMap&libraries=marker&loading=async">
-</script>
-<script>
-    // Function to initialize the map
-    function initMap() {
-        // Center the map on Uganda
-        var uganda = {
-            lat: 1.3733,
-            lng: 32.2903
-        };
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 6,
-            center: uganda
-        });
-
-        // Generate and place 10 random markers within Uganda
-        for (var i = 0; i < 10; i++) {
-            var lat = getRandomInRange(-1.5, 4, 6);
-            var lng = getRandomInRange(29.5, 35, 6);
-
-            var markerElement = document.createElement('div');
-            markerElement.className = 'custom-marker';
-            markerElement.style.backgroundImage =
-                'url("https://maps.gstatic.com/intl/en_us/mapfiles/marker_green.png")';
-            markerElement.style.backgroundSize = 'contain';
-            markerElement.style.width = '20px';
-            markerElement.style.height = '34px';
-
-            new google.maps.marker.AdvancedMarkerElement({
-                map: map,
-                position: {
-                    lat: lat,
-                    lng: lng
-                },
-                content: markerElement
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Random GPS Markers in Uganda</title>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC9Mdls_ETVjOb_u5bjcqavSI4E8S1D2Vs&callback=initMap"></script>
+    <script>
+        // Function to initialize the map
+        function initMap() {
+            // Center the map on Uganda
+            var uganda = {lat: 1.3733, lng: 32.2903};
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 6,
+                center: uganda
             });
-        }
-    }
 
-    // Function to get a random number within a range
-    function getRandomInRange(from, to, fixed) {
-        return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
-    }
-</script>
-<style>
-    .custom-marker {
-        position: absolute;
-        transform: translate(-50%, -50%);
-    }
-</style>
-<h1>Random GPS Markers in Uganda</h1>
-<div id="map" style="height: 500px; width: 100%;"></div>
+            // Generate and place 10 random markers within Uganda
+            for (var i = 0; i < 10; i++) {
+                var lat = getRandomInRange(-1.5, 4, 6);
+                var lng = getRandomInRange(29.5, 35, 6);
+                var markerElement = document.createElement('div');
+                markerElement.textContent = '📍';
+                markerElement.style.fontSize = '24px';
+                markerElement.style.position = 'absolute';
+                markerElement.style.transform = 'translate(-50%, -50%)';
+
+                new google.maps.marker.AdvancedMarkerElement({
+                    map: map,
+                    position: {lat: lat, lng: lng},
+                    content: markerElement
+                });
+            }
+        }
+
+        // Function to get a random number within a range
+        function getRandomInRange(from, to, fixed) {
+            return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
+        }
+    </script>
+</head>
+<body>
+    <h1>Random GPS Markers in Uganda</h1>
+    <div id="map" style="height: 500px; width: 100%;"></div>
+</body>
+</html>
+``
