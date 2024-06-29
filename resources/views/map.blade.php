024c30f5a6fea19ecb5c8cd4e3eb33bc2f8a1dc3
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-06-29 11:29:05 +0300

    Refactor map.blade.php to adjust map height to 600px

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -21,7 +21,7 @@
         </div>
     </div>
 </div>
-<div id="map" style="width: 100%; height: 650px;"></div>
+<div id="map" style="width: 100%; height: 600px;"></div>
 <script>
     //document ready
 
