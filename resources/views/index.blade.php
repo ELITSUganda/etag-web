d052e13c6ba6d8f28d91defa96b27a503bfc9817
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-07-25 23:56:32 +0300

    Refactor page title meta tags for better SEO optimization

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>U-LITS &#8211; Ensuring Quality through Traceability</title>
+    <title>U-LITS &#8211; Ensuring Quality through Traceability.</title>
     <link rel="icon" href="https://about.u-lits.com/wp-content/uploads/2024/06/cropped-logo-1-32x32.png"
         sizes="32x32" />
     <link rel="icon" href="https://about.u-lits.com/wp-content/uploads/2024/06/cropped-logo-1-192x192.png"
@@ -15,21 +15,18 @@
     <meta name="msapplication-TileImage"
         content="https://about.u-lits.com/wp-content/uploads/2024/06/cropped-logo-1-270x270.png" />
     {{-- Desscription meta  --}}
-    <meta name="description"
-        content="U-LITS is a software development company that specializes in developing software solutions for businesses and organizations.">
+    <meta name="description" content="Uganda Livestock Traceability & Tracking System.">
     {{-- Open Graph / Facebook  --}}
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://about.u-lits.com/">
-    <meta property="og:title" content="U-LITS &#8211; Ensuring Quality through Traceability">
-    <meta property="og:description"
-        content="U-LITS is a software development company that specializes in developing software solutions for businesses and organizations.">
+    <meta property="og:title" content="U-LITS &#8211; Ensuring Quality through Traceability.">
+    <meta property="og:description" content="Uganda Livestock Traceability & Tracking System.">
     <meta property="og:image" content="https://about.u-lits.com/wp-content/uploads/2024/06/cropped-logo-1-270x270.png">
     {{-- Twitter  --}}
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="https://about.u-lits.com/">
-    <meta property="twitter:title" content="U-LITS &#8211; Ensuring Quality through Traceability">
-    <meta property="twitter:description"
-        content="U-LITS is a software development company that specializes in developing software solutions for businesses and organizations.">
+    <meta property="twitter:title" content="U-LITS &#8211; Ensuring Quality through Traceability.">
+    <meta property="twitter:description" content="Uganda Livestock Traceability & Tracking System.">
     <meta property="twitter:image"
         content="https://about.u-lits.com/wp-content/uploads/2024/06/cropped-logo-1-270x270.png">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
@@ -37,9 +34,8 @@
     <meta property="og:site_name" content="U-LITS">
     <meta property="og:locale" content="en_US">
     <meta property="og:type" content="website">
-    <meta property="og:title" content="U-LITS &#8211; Ensuring Quality through Traceability">
-    <meta property="og:description"
-        content="U-LITS is a software development company that specializes in developing software solutions for businesses and organizations.">
+    <meta property="og:title" content="U-LITS &#8211; Ensuring Quality through Traceability.">
+    <meta property="og:description" content="Uganda Livestock Traceability & Tracking System.">
     <meta property="og:url" content="https://about.u-lits.com/">
     <meta property="og:image" content="https://about.u-lits.com/wp-content/uploads/2024/06/cropped-logo-1-270x270.png">
     <meta property="og:image:secure_url"
@@ -47,7 +43,7 @@
     <meta property="og:image:width" content="270">
     <meta property="og:image:height" content="270">
     <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content="U-LITS &#8211; Ensuring Quality through Traceability">
+    <meta name="twitter:title" content="U-LITS &#8211; Ensuring Quality through Traceability.">
 </head>
 
 <body>
