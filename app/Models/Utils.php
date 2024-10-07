2025de360ad96f07652531e5358615e25400a4d9
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-10-08 00:06:13 +0300

    as

--- app/Models/Utils.php
+++ app/Models/Utils.php
@@ -2131,7 +2131,7 @@ duplicate_results
     {
         $_100_animals = Animal::where([])
             ->latest()
-            ->limit(1000)
+            ->limit(500)
             ->get();
         foreach ($_100_animals as $key => $animal) {
             $animal->details .= ".";
