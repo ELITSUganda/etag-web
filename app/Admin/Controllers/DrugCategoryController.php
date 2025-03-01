840451c3597a49dda4628114bb64ca98f870bb17
Muhindo Mubaraka <mubahood360@gmail.com>
2025-03-01 12:55:44 +0300

    as

--- app/Admin/Controllers/DrugCategoryController.php
+++ app/Admin/Controllers/DrugCategoryController.php
@@ -27,7 +27,7 @@ class DrugCategoryController extends AdminController
         $grid = new Grid(new DrugCategory());
         $grid->disableBatchActions();
         $grid->quickSearch('name')->placeholder('Search Drug Category');
-        $grid->column('id', __('Id'));
+        $grid->column('id', __('Id'))->hide();
         $grid->picture('photo', __('Photo'))->sortable();
         $grid->column('name', __('Name'))->sortable();
         $grid->column('unit', __('Unit'))->sortable(); 
