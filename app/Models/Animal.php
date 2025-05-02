33500dc3a7103c4161b23272c648ad21914a9ec4
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2025-05-02 17:40:06 +0300

    as

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -34,7 +34,6 @@ class Animal extends Model
         parent::boot();
         self::creating(function ($model) {
 
-
             $animal = Animal::where('e_id', $model->e_id)->first();
             if ($animal != null) {
                 throw new Exception("Animal with same elecetronic ID ($model->e_id) aready exist in the system.", 1);
@@ -126,6 +125,22 @@ class Animal extends Model
             if (isset($model->photos)) {
                 unset($model->photos);
             }
+
+            //check for duplicate of v_id or e_id
+            if($model->v_id != null && strlen($model->v_id) > 3){
+                $animal = Animal::where('v_id', $model->v_id)->where('id', '!=', $model->id)->first();
+                if ($animal != null) {
+                    throw new Exception("Animal with same v-ID ($model->v_id) aready exist in the system.");
+                    return false;
+                }
+            }
+            if($model->e_id != null && strlen($model->e_id) > 3){
+                $animal = Animal::where('e_id', $model->e_id)->where('id', '!=', $model->id)->first();
+                if ($animal != null) {
+                    throw new Exception("Animal with same elecetronic ID ($model->e_id) aready exist in the system.", 1);
+                    return false;
+                }
+            } 
 
             $f = Farm::find($model->farm_id);
             if ($f == null) {
