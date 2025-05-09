cf9fd32a1ccfb05832f847e2fd5fc13d63c77b27
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2025-05-09 23:53:48 +0300

    as

--- app/Http/Controllers/ApiFarmController.php
+++ app/Http/Controllers/ApiFarmController.php
@@ -156,13 +156,12 @@ class ApiFarmController extends Controller
                         break;
                     }
                 }
+                $data = Farm::where($where)->get();
+            } else {
+                //where owner is in the list of access_ids
+                $data = Farm::whereIn('administrator_id', $access_ids)->get();
             }
-            $data = Farm::where($where)->get();
-        } else {
-            //where owner is in the list of access_ids
-            $data = Farm::whereIn('administrator_id', $access_ids)->get();
         }
-
 
         return Utils::response([
             'status' => 1,
